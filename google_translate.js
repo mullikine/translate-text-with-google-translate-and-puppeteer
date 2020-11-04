3310aaf18c5bbfeb0e2caf0429ce490c74710833
Shane Mulligan <mullikine@gmail.com>
2020-11-04 17:04:13 +1300

    Wed 04 Nov 2020 17:04:13 NZDT

--- google_translate.js
+++ google_translate.js
@@ -2,19 +2,19 @@ const puppeteer = require('puppeteer');
 
 (async () => {
 	let launchOptions = { headless: false, args: ['--start-maximized'] };
-	
+
 	const browser = await puppeteer.launch(launchOptions);
 	const page = await browser.newPage();
-	
+
 	// set viewport and user agent (just in case for nice viewing)
 	await page.setViewport({width: 1366, height: 768});
 	await page.setUserAgent('Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36');
-	
+
 	// define source and target language code
 	let sourceLang = 'id', targetLang = 'en';
 
 	await page.goto(`https://translate.google.com/#view=home&op=translate&sl=${sourceLang}&tl=${targetLang}`);
-	
+
 	// detect the source textarea for input data (source string)
 	await page.waitForSelector('#source');
 	await page.waitFor(1000);
@@ -34,7 +34,7 @@ const puppeteer = require('puppeteer');
 
 	// display the source and translated text to console
 	console.log(`${sourceLang}: ${sourceString}\n${targetLang}: ${translatedResult}`);
-	
+
 	await page.waitFor(1000);
 	await browser.close();
 })();
